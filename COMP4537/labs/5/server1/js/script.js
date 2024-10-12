1cb575c4df151138f0b3386d15ac613dcbc9274b
Harrison de Jong <harrisondijon@gmail.com>
2024-10-11 23:26:16 -0700

    Added needed functions and endpoint

--- COMP4537/labs/5/server1/js/script.js
+++ COMP4537/labs/5/server1/js/script.js
@@ -7,6 +7,7 @@ import {messages} from "./lang/messages/en/user.js";
 const http = require('http');
 const url = require('url');
 const {MESSAGES, QUERIES} = require('./lang/en/userInfo.js');
+const REQUEST_URL = 'https://leewillin.store/lab5/api/v1/sql';
 
 const insertButton = document.getElementById('insertRowsButton');
 const sqlQueryInput = document.getElementById('sqlQueryInput');
@@ -15,3 +16,25 @@ const resultTable = document.getElementById('resultTable');
 
 insertButton.addEventListener('click', insertRows);
 submitButton.addEventListener('click', handleQuery);
+
+async function insertRows(){
+    const query = QUERIES.insertQuery;
+}
+
+async function sendPostQuery(){
+    
+}
+
+async function fetchData(){
+
+}
+
+function displayResponse(){
+
+}
+
+function populateTable(){
+
+}
+
+
