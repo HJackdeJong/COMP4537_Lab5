acc871bbe5ae42a46e2a4d556f918542a10d0d84
Harrison de Jong <harrisondijon@gmail.com>
2024-10-11 22:54:25 -0700

    Fixed file structure for lab requirements, removed server2 directory and contents

---
+++ COMP4537/labs/5/server1/js/script.js
@@ -0,0 +1,6 @@
+// This project was created with the assistance of ChatGPT model 3.5
+// in editing code and providing feedback
+
+// import {messages} from "COMP4537/lab/5/server1/lang/messages/en/en.js";
+import {messages} from "./lang/messages/en/user.js";
+
