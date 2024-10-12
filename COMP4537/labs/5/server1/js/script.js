fec472cd9febb45c45c4f54078b5c52b9eb733dd
Harrison de Jong <harrisondijon@gmail.com>
2024-10-11 23:16:54 -0700

    Added missing submit button to html file, added consts and event listeners to script.js, and added user-facing strings to user.js including the query for the "insert" button

--- COMP4537/labs/5/server1/js/script.js
+++ COMP4537/labs/5/server1/js/script.js
@@ -4,3 +4,14 @@
 // import {messages} from "COMP4537/lab/5/server1/lang/messages/en/en.js";
 import {messages} from "./lang/messages/en/user.js";
 
+const http = require('http');
+const url = require('url');
+const {MESSAGES, QUERIES} = require('./lang/en/userInfo.js');
+
+const insertButton = document.getElementById('insertRowsButton');
+const sqlQueryInput = document.getElementById('sqlQueryInput');
+const submitButton = document.getElementById('submitButton');
+const resultTable = document.getElementById('resultTable');
+
+insertButton.addEventListener('click', insertRows);
+submitButton.addEventListener('click', handleQuery);
---
+++ COMP4537/labs/5/server1/lang/messages/en/user.js
@@ -0,0 +1,11 @@
+export const MESSAGES = {
+
+}
+
+export const QUERIES = {
+    insertQuery: `INSERT INTO patient (name, birthdate) VALUES
+    ('Sara Brown, '1901-01-01'),
+    ('John Smith', '1941-01-01'),
+    ('Jack Ma', '1961-01-30'),
+    ('Elon Musk', '1999-01-01')`,
+};
